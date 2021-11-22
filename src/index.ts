17f194ae588df46eb0a7978599733568f7b6e0aa
Jon Andoni <jonandonigv@gmail.com>
2021-11-22 20:03:55 +0100

    dotenv config and db connection

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
-dotenv.config({path: __dirname + '../.env'});
+dotenv.config({path: '.env'});
+console.log(__dirname);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT!;
+const DB = process.env['DB_URI']!;
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -19,5 +22,10 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`[SERVER]: Server is running at https://localhost:${PORT}`);
+    console.log(`[SERVER]: Server is running at http://localhost:${PORT}`);
 });
+console.log(DB, PORT);
+// Db connection
+mongoose.connect(DB).then(()=> {
+    console.log('[SERVER] Database connection success.')
+}).catch(e => console.log(`Can't connect to the db`));
