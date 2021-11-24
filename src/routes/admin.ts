d776c38025e8242c7c30d9d9743b1d74ecf0fbae
Jon Andoni <jonandonigv@gmail.com>
2021-11-24 16:54:35 +0100

    routes created

---
+++ src/routes/admin.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import {body} from 'express-validator';
+
+import * as authController from '../controllers/auth';
+import { User } from "../models/user";
+const router = Router();
+router.put('/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email')
+        .custom((value, {req})=> {
+            return User.findOne({email: value}).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('Email address already exist');
+                }
+            });
+        })
+        .normalizeEmail(),
+    body('password')
+        .trim()
+        .isLength({min: 6}),
+    body('name')
+        .trim()
+        .not()
+        .isEmpty()
+], authController.SingUp);
+
+export default router;
