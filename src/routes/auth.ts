a3a8dd9c520b2773acdb7bab6040c23aff7eb17a
Jon Andoni <jonandonigv@gmail.com>
2021-11-24 14:42:05 +0100

    Add validation to the signup route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -2,9 +2,28 @@ import { Router } from "express";
 import {body} from 'express-validator';
 
 import * as authController from '../controllers/auth';
+import { User } from "../models/user";
 const router = Router();
-
-router.post('/signup', authController.SingUp);
+router.put('/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Please enter a valid email')
+        .custom((value, {req})=> {
+            return User.findOne({email: value}).then(userDoc => {
+                if (userDoc) {
+                    return Promise.reject('Email address already exist');
+                }
+            });
+        })
+        .normalizeEmail(),
+    body('password')
+        .trim()
+        .isLength({min: 6}),
+    body('name')
+        .trim()
+        .not()
+        .isEmpty()
+], authController.SingUp);
 // router.post('/Login', authController.postLogin);
 
 export default router;
