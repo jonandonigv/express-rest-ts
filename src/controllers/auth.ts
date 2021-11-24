4ca38610cfeece401bc3ef700e3969979c284493
Jon Andoni <jonandonigv@gmail.com>
2021-11-24 18:22:02 +0100

    Login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import { validationResult } from "express-validator";
 
-import {User} from '../models/user';
+import {User, UserDocument} from '../models/user';
 
 
 export const SingUp = async (req: Request, res: Response, next: NextFunction) => {
@@ -38,7 +38,28 @@ export const SingUp = async (req: Request, res: Response, next: NextFunction) =>
 };
 
 export const Login = async (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).json({msg: 'Success'});
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    try {
+        const user = await User.findOne({email: email});
+        if(!user) {
+            res.status(401).json({message: 'A user with this email could not be found'});
+        }
+        loadedUser = user;
+        const isEqual = bcrypt.compare(password, loadedUser?.password!);
+        if (!isEqual) {
+            res.status(401).json({message: 'Wrong password'});
+        }
+        const token = jwt.sign({
+            email: loadedUser?.email!,
+            userId: loadedUser?._id.toString()
+        }, 'somesecret',
+        {expiresIn: '1h'});
+        res.status(200).json({token: token, userId: loadedUser?._id.toString()});
+    } catch (error) {
+        if (error) {
+            res.status(500).json({error: error});
+        }
+    }
 };
-
-export const LogOff = async (req: Request, res: Response, next: NextFunction) => {}
