97df6e8efc1ebe28c5e84fc5724d60da15e59fd5
Jon Andoni <jonandonigv@gmail.com>
2021-11-24 16:54:58 +0100

    Rearrange a lot of stuff and now it work as intended

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-import express, { NextFunction, ErrorRequestHandler } from 'express';
+import path from 'path';
+import express, { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import csrf from 'csurf';
 import MongoStore from 'connect-mongo';
 import multer from 'multer';
-import session from 'express-session';
 
 // route imports
 import authRoutes from  './routes/auth';
-import path from 'path/posix';
+import adminRoutes from './routes/admin';
+import shopRoutes from './routes/shop';
+
 
 dotenv.config({path: '.env'});
-console.log(__dirname);
-
-const app = express();
 const PORT = process.env.PORT!;
 const DB = process.env['DB_URI']!;
 
+const app = express();
 const store = new MongoStore({
     mongoUrl: DB,
     collectionName: 'Session'
 });
-// File management
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    console.log(err);
+    const status = err.statusCode || 500;
+    const message = err.message;
+    const data = err.data;
+    res.status(status).json({message: message, data: data});
+};
+
 const fileStorage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'images');
@@ -31,64 +37,44 @@ const fileStorage = multer.diskStorage({
     }
 });
 
-// Express configuration
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: false
-}));
-app.use(csrf());
-app.use(multer({storage: fileStorage, fileFilter: (req, file, cb) => {
-    if (
-        file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg'
-    ) {
-        cb(null, true);
-    } else {
-        cb(null, false);
+app.use(multer({
+    storage: fileStorage,
+    fileFilter: (req, file, cb) => {
+        if (
+            file.mimetype === 'image/png' || 
+            file.mimetype === 'image/jpg' ||
+            file.mimetype === 'image/jpeg'
+        ) {
+            cb(null, true)
+        } else {
+            cb(null, false)
+        }
     }
-}}).single('image'));
+}).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use(session({
+// In theory in RESTapis you don't have sessions because all the session like things happen in the client.
+/* app.use(session({
     secret: 'my secret',
     resave: false,
     saveUninitialized: false,
     store: store
-}));
+})); */
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-});
-
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    console.log(err);
-    const status = err.statusCode || 500;
-    const message = err.message;
-    const data = err.data;
-    res.status(status).json({message: message, data: data});
-};
-app.use(errorHandler);;
-// Routes
-
-/* app.use((req, res, next) => {
-    res.locals.isAuthenticated = req.session.isLoggedIn;
-    res.locals.csrfToken = req.csrfToken();
     next();
 });
 
-app.use((req, res, next) => {
-    if (!req.session.user) {
-        return next();
-    }
-    // TODO: Search for user logged in and if the user does not exist throw error
-}); */
-
-app.get('/', (req, res) => {
-    res.status(200).json({
-        error: null,
-        msg: 'Api running'
-    });
-});
 app.use('/auth', authRoutes);
+app.use('/admin', adminRoutes);
+app.use('/shop', shopRoutes);
+
+
+
+
+app.use(errorHandler);
 
 // Server initialization
 app.listen(PORT, () => {
@@ -96,5 +82,5 @@ app.listen(PORT, () => {
 });
 // Db connection
 mongoose.connect(DB).then(()=> {
-    console.log('[SERVER] Database connection success.')
+    console.log('[SERVER]: Database connection success.')
 }).catch(e => console.log(`Can't connect to the db`));
