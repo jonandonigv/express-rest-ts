250fc15fb0c6c9c5ea04a67081e475cd513a99a2
Jon Andoni <jonandonigv@gmail.com>
2021-11-26 20:06:54 +0100

    addToCart method added

--- src/models/user.ts
+++ src/models/user.ts
@@ -31,4 +31,28 @@ const userSchema = new mongoose.Schema<UserDocument>(
     }
 );
 
+userSchema.methods.addToCart = function(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+        return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity;
+    const updatedCartItems = [...this.cart.items];
+
+    if (cartProductIndex >= 0) {
+        newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+        updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+        updatedCartItems.push({
+            productId: product._id,
+            quantity: newQuantity
+        });
+    }
+
+    const updatedCart = {
+        items: updatedCartItems
+    };
+    this.cart = updatedCart;
+    return this.save();
+};
+
 export const User = mongoose.model<UserDocument>("User", userSchema);
